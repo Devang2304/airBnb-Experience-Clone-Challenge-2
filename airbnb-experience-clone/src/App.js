062eb2b36b2c83dd46cafe8d18bfc143d33ccd79
Devang2304 <devangvartak2304@gmail.com>
2022-10-28 17:44:37 +0530

    created Navbar

--- airbnb-experience-clone/src/App.js
+++ airbnb-experience-clone/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className="App">
-       <h1>Learn React</h1>
+    <div>
+       <Navbar/>
     </div>
   );
 }
---
+++ airbnb-experience-clone/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import airbnbimage from "./images/airbnbimage.png";
+
+export default  function Navbar() {
+    return (
+        <nav>
+            <img src={airbnbimage}/>
+        </nav>
+    )
+} 
